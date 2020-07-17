2090b5d9d45601a1e6d84741c29d0657ad60d60c
TJun-Jie <tjunjie2000@gmail.com>
2020-07-17 18:42:50 +0800

    Added routing

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const data = require('./public/data.json');
+
+
+const app = express();
+
+const router = require('./routes/mainRoutes');
+app.set('view engine' , 'pug');
+app.use('/static', express.static('public'))
+
+app.use(router)
+
+
+
+
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('App listening on port 3000')
+})

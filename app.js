9f69087ada425c6f154b157858ace50f9397fa90
TJun-Jie <tjunjie2000@gmail.com>
2020-07-18 02:16:52 +0800

    Added code comments

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 const express = require('express');
 const data = require('./public/data.json');
-
-
 const app = express();
-
 const router = require('./routes/mainRoutes');
+
 app.set('view engine' , 'pug');
+
+// set static route in the public directory
 app.use('/static', express.static('public'))
 
+// link main routes to app.js
 app.use(router)
 
 // 404 error if no route is found
 app.use((req, res , next) => {
     const err =  new Error('Sorry page could not be found')
     err.status = 404;
+    // pass error to global error handler
     next(err)
 })
 
+// Global error handler
 app.use( (err, req, res, next) => {
     // catch other errors other than 404
     if(!err.status) {
         err.status = 500;
     }
     res.status(err.status);
+    // Render error page
     res.render('error', {err: err})
 })
 
-
-
-
 const port = 3000;
-
+// Starts a server and listen on port 3000 locally
 app.listen(port, () => {
     console.log(`App listening on port ${port} at localhost:${port}`)
 })
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,21 +4,23 @@ const projects = require('../public/data.json').projects;
 
 const router = express.Router();
 
+// index route
 router.get('/', (req, res)  => {
     res.render('index', {projects})
 
 })
 
+// about route
 router.get('/about',  (req, res) => {
     res.render('about')
 })
 
-
+// Projects route
 router.get('/projects/:id',  (req, res) => {
     const id  = req.params.id
     res.render('project', {projects: projects, id: id})
 })
 
 
-
+// export this route to app.js
 module.exports = router;

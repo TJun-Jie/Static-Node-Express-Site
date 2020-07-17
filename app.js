2327f65415137da7c9552bd1a0db115fdc19d9d6
TJun-Jie <tjunjie2000@gmail.com>
2020-07-17 18:49:29 +0800

    Added npm start

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ app.use(router)
 
 
 
+
 const port = 3000;
 
 app.listen(port, () => {
-    console.log('App listening on port 3000')
+    console.log(`App listening on port ${port} at localhost:${port}`)
 })
